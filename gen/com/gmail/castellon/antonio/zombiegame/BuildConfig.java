63271f73738ad9dcca8950dacd0f7d7a1d3a2998
Antonio Castellon <antonio.castellon@gmail.com>
2015-02-17 14:20:38 +0100

    adapt to the new Android Studio

--- gen/com/gmail/castellon/antonio/zombiegame/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gmail.castellon.antonio.zombiegame;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
